605825231e8da374329572b9003c6c61757aa0b7
thayn <thayn@Thay>
2022-03-13 19:30:25 -0300

    Exercicio3

---
+++ src/Aula3/Main.java
@@ -0,0 +1,15 @@
+package Aula3;
+
+	import Aula3.Controller2;
+	public class Main {
+
+		public static void main(String[] args) {
+		
+			for (int idthread = 0 ; idthread < 5 ; idthread++) {
+			Thread printthread = new Thread();
+			printthread.start();
+			
+			}
+
+		}
+}
